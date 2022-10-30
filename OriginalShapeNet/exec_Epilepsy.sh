d202896c3dfec38e5fe32210ff19ba918f379961
Duy Tran <duy.thd@kyanon.digital>
2022-10-30 13:33:01 +0700

    all

---
+++ OriginalShapeNet/exec_Epilepsy.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+python3 uea_original.py --dataset Epilepsy --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 0.5
+python3 uea_original.py --dataset Epilepsy --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 0.7
+python3 uea_original.py --dataset Epilepsy --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 0.1
+python3 uea_original.py --dataset Epilepsy --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 0.3
+python3 uea_original.py --dataset Epilepsy --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 1
