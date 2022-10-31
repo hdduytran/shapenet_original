d3632de1d6773a8ce272b7123f506cd03fbff309
Duy Tran <duy.thd@kyanon.digital>
2022-10-31 20:07:57 +0700

    train

--- OriginalShapeNet/exec.sh
+++ OriginalShapeNet/exec.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
-python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 0.5
-python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 0.7
-python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 0.1
-python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 0.3
-python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ../../shapenet_results --cuda --hyper default_parameters.json --ratio 1
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.1
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.2
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.3
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.4
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.5
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.6
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.7
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.8
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 0.9
+python3 uea_original.py --dataset BasicMotions --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json --ratio 1
