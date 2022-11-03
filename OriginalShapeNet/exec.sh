23d2ab7d845c808d973f30201bfac52611535b95
Duy Tran <duy.thd@kyanon.digital>
2022-11-03 16:40:24 +0700

    train

--- OriginalShapeNet/exec.sh
+++ OriginalShapeNet/exec.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 dataset=$1
 
-python3 uea_original.py --dataset ${dataset} --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json
+python3 uea_original.py --dataset ${dataset} --gpu 1 --path ../../dataset --save_path ./shapenet_results --cuda --hyper default_parameters.json
